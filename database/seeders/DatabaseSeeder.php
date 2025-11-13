1c7bbabf22b307a75d153631834a1fbf4f429c97
AngeloPapas <papasangelo97@gmail.com>
2025-11-13 12:03:50 +0800

    Add DepartmentSeeder to DatabaseSeeder for initial data population

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             RolePermissionSeeder::class,
+            DepartmentSeeder::class,
         ]);
 
         $user = User::factory()->create([
