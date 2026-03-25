f02c9e2ec972f6c9ea3d09a21d657233339a4fd0
AngeloPapas <papasangelo97@gmail.com>
2026-03-25 22:48:19 +0800

    Add RequestItem model with fillable attributes and relationships

---
+++ app/Models/RequestItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class RequestItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'request_id', 'item_id', 'quantity', 'particular', 'unit'
+    ];
+
+    public function request()
+    {
+        return $this->belongsTo(Request::class);
+    }
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
+}
