bcbbc85b0e289d9d40721523971f8d9bd4be1902
AngeloPapas <papasangelo97@gmail.com>
2026-05-19 10:23:01 +0800

    chore(seeders): remove unused Hash import from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Support\Facades\Hash;
-
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
